14a8c012f6643d8342d3936795d6490d9c066d6f
Egor Merkushev <egor.merkushev@yandex.ru>
2025-05-04 23:31:25 +0300

    Add SwiftUI App project from Xcode template

---
+++ GCDTaskInline/GCDTaskInlineApp.swift
@@ -0,0 +1,17 @@
+//
+//  GCDTaskInlineApp.swift
+//  GCDTaskInline
+//
+//  Created by Egor Merkushev on 04.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct GCDTaskInlineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
